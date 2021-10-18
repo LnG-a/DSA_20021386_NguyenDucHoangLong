21e5506f94b92bdf8bed7d0b9a269f21be6ccc13
LnG <pleasewaitlong@gmail.com>
2021-10-18 20:49:31 +0700

    week6
    4 Sort Algorithms

---
+++ week6/src/Sort.java
@@ -0,0 +1,146 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Insertion;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Sort {
+    public static void main(String[] args) {
+        In myIn = new In("100kAscending.txt");
+        In myIn1 = new In("100kDescending.txt");
+        In myIn2 = new In("100kRandom.txt");
+        In myIn3 = new In("100kFewUnique.txt");
+        long start;
+        long end;
+        double tong=0;
+
+        int[] a = myIn.readAllInts();
+        int[] b=new int[a.length];
+        int n =5;
+        for (int j=0;j<n;j++){
+            for (int i=0;i<a.length;i++){
+                b[i]=a[i];
+            }
+            start = System.currentTimeMillis();
+            selectionSort(b);
+            end = System.currentTimeMillis();
+            tong+=(end-start);
+        }
+        System.out.println(tong/n+"ms");
+    }
+
+    public static void selectionSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i+1; j < a.length; j++) {
+                if (a[min] > a[j]) min = j;
+            }
+            exchange(a, i, min);
+        }
+    }
+
+    public static void insertionSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (a[j] < a[j - 1]) {
+                    exchange(a, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void exchange(int[] a, int i, int j) {
+        int exchange = a[i];
+        a[i] = a[j];
+        a[j] = exchange;
+    }
+
+    public static void mergeSort(int[] a) {
+        int[] aux = new int[a.length];
+        sort(a, aux, 0, a.length - 1);
+
+    }
+
+    private static void sort(int[] a, int[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    private static void merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (aux[j] < aux[i]) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    public static void quickSort(int[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static int partition(int[] a, int lo, int hi) {
+        if (hi<=lo) return hi;
+        int i = lo;
+        int j = hi + 1;
+        while (true) {
+            while (a[++i] < a[lo]) {
+                if (i == hi) break;
+            }
+            while (a[--j] > a[lo]) {
+                if (j == lo) break;
+            }
+
+            if (i >= j) break;
+            exchange(a, i, j);
+        }
+        exchange(a, lo, j);
+        return j;
+    }
+
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+}
+/*
+* Time (ms)
+*
+*       |           |Selection sort |Insertion sort |  Merge sort   |  Quick sort   |
+* data  |   type    |               |               |               |               |
+* 1k    |Ascending  |     4.6       |     0.0       |     0.4       |     2.6       |
+*       |Descending |     5.2       |     9.8       |     0.6       |     2.8       |
+*       |Random     |     4.4       |     4.6       |     1.0       |     3.4       |
+*       |FewUnique  |     5.2       |     5.8       |     0.8       |     3.0       |
+* -----------------------------------------------------------------------------------
+* 32k   |Ascending  |    136.6      |     1.2       |     8.0       |    16.4       |
+*       |Descending |    721.4      |    392.2      |     7.2       |    15.0       |
+*       |Random     |    392.4      |    195.2      |     5.6       |     6.6       |
+*       |FewUnique  |    404.2      |    193.6      |     6.4       |     6.8       |
+* -----------------------------------------------------------------------------------
+* 100k  |Ascending  |    1199.4     |     2.0       |     7.0       |    16.8       |
+*       |Descending |    3836.6     |    3558.0     |     7.4       |    18.0       |
+*       |Random     |    4159.4     |    1786.6     |    15.4       |    16.2       |
+*       |FewUnique  |    4177.2     |    1766.8     |    15.0       |    14.8       |
+* -----------------------------------------------------------------------------------
+*
+* Quicksort without shuffle will lead to StackOverFlow with data:
+* - about 16570 int ascending
+* - about 16000 int descending
+*/
---
+++ week6/src/TestWriter.java
@@ -0,0 +1,75 @@
+import edu.princeton.cs.algs4.Out;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestWriter {
+    public static void ascending(String path, int size) {
+        List<Integer> a=new ArrayList<>();
+        for (int i=1;i<=size;i++){
+            a.add(i);
+        }
+        writeFile(path,a);
+    }
+
+    public static void descending(String path, int size) {
+        List<Integer> a=new ArrayList<>();
+        for (int i=size;i>=1;i--){
+            a.add(i);
+        }
+        writeFile(path,a);
+    }
+
+    public static void random(String path, int size) {
+        List<Integer> a=new ArrayList<>();
+        for (int i=size;i>=1;i--){
+            a.add(StdRandom.uniform(size));
+        }
+        writeFile(path,a);
+    }
+
+    public static void fewUnique(String path, int size) {
+        List<Integer> a=new ArrayList<>();
+        for (int i=size;i>=1;i--){
+            a.add(StdRandom.uniform(size/100));
+        }
+        writeFile(path,a);
+    }
+
+
+    private static void writeFile(String path, List<Integer> a) {
+        try {
+        File del = new File(path);
+        del.delete();
+        File dictFile = new File(path);
+        dictFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        Out printer = new Out(path);
+        for (int i=0;i<a.size();i++){
+            printer.println(a.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        ascending("32kAscending.txt",32000);
+        descending("32kDescending.txt",32000);
+        random("32kRandom.txt",32000);
+        fewUnique("32kFewUnique.txt",32000);
+        ascending("1kAscending.txt",1000);
+        descending("1kDescending.txt",1000);
+        random("1kRandom.txt",1000);
+        fewUnique("1kFewUnique.txt",1000);
+        ascending("100kAscending.txt",100000);
+        descending("100kDescending.txt",100000);
+        random("100kRandom.txt",100000);
+        fewUnique("100kFewUnique.txt",100000);
+        /*ascending("2kAscending.txt",16568);
+        descending("2kDescending.txt",16001);*/
+    }
+}
