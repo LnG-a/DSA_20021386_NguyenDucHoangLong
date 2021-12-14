36f195df76592f01b32edef9eaf9981e1e8120e4
LnG <pleasewaitlong@gmail.com>
2021-12-14 18:43:06 +0700

    update all homework because teacher check FAILED

--- week2/src/BTTL/B.java
+++
@@ -1,8 +0,0 @@
-
-public class B {
-    public static void main(String[] args) {
-        int x=1,y=2;
-        System.out.println(A.sum(x,y));
-        System.out.println(A.sum(A.square(x),A.square(y)));
-    }
-}
